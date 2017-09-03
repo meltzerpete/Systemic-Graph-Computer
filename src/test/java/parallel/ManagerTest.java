d58efbe68f219d9b0e6561b9495c5427701da7ed
Pete Meltzer <meltzerpete@gmail.com>
2017-09-03 22:20:28 +0100

    tidy

--- src/test/java/parallel/ManagerTest.java
+++ src/test/java/parallel/ManagerTest.java
@@ -1,6 +1,6 @@
 package parallel;
 
-import graphEngine.Components;
+import graphEngine.TestGraphQueries;
 import org.junit.Rule;
 import org.junit.Test;
 import org.neo4j.driver.v1.Config;
@@ -8,12 +8,9 @@ import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.GraphDatabase;
 import org.neo4j.driver.v1.Session;
 import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Label;
-import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.harness.junit.Neo4jRule;
 
-import static graphEngine.TestGraphQueries.viewGraph;
 
 /**
  * Created by Pete Meltzer on 05/08/17.
@@ -30,143 +27,18 @@ public class ManagerTest {
                 .toConfig());
              Session session = driver.session()) {
             
-//            neo4j.withConfig("LockManager", "community");
             GraphDatabaseService db = neo4j.getGraphDatabaseService();
 
             // create program
             int noOfDataSystems = 50;
             try (Transaction tx = db.beginTx()) {
 
-                // scopes
-                Node main = db.createNode(Components.SCOPE);
-                Node computation = db.createNode(Components.SCOPE, Components.COMPUTATION);
-
-                // contexts
-                Node initializer = db.createNode(Components.CONTEXT);
-                initializer.setProperty(Components.function, "INITIALIZE");
-                initializer.setProperty(Components.s1Query, "(:Uninitialized)");
-                initializer.setProperty(Components.s2Query, "(:Computation)");
-
-                Node binMutate = db.createNode(Components.CONTEXT);
-                binMutate.setProperty(Components.function, "BINARYMUTATE");
-                binMutate.setProperty(Components.s1Query, "(:Initialized)");
-                binMutate.setProperty(Components.s2Query, "(:Initialized)");
-
-                Node onePointCross = db.createNode(Components.CONTEXT);
-                onePointCross.setProperty(Components.function, "ONEPOINTCROSS");
-                onePointCross.setProperty(Components.s1Query, "(:Initialized)");
-                onePointCross.setProperty(Components.s2Query, "(:Initialized)");
-
-                Node uniformCross = db.createNode(Components.CONTEXT);
-                uniformCross.setProperty(Components.function, "UNIFORMCROSS");
-                uniformCross.setProperty(Components.s1Query, "(:Initialized)");
-                uniformCross.setProperty(Components.s2Query, "(:Initialized)");
-
-                Node output = db.createNode(Components.CONTEXT);
-                output.setProperty(Components.function, "OUTPUT");
-                output.setProperty(Components.s1Query, "(:Fittest)");
-                output.setProperty(Components.s2Query, "(:Initialized)");
-
-                main.createRelationshipTo(output, Components.CONTAINS);
-                main.createRelationshipTo(initializer, Components.CONTAINS);
-                main.createRelationshipTo(computation, Components.CONTAINS);
-
-                computation.createRelationshipTo(binMutate, Components.CONTAINS);
-                computation.createRelationshipTo(onePointCross, Components.CONTAINS);
-                computation.createRelationshipTo(uniformCross, Components.CONTAINS);
-
-                // data nodes
-                for (int i = 0; i < noOfDataSystems; i++) {
-                    Node data = db.createNode(Components.DATA, Components.UNINITIALIZED);
-                    main.createRelationshipTo(data, Components.CONTAINS);
-                }
-
-                // fittest solution
-                Node fittest = db.createNode(Components.DATA, Components.FITTEST);
-                main.createRelationshipTo(fittest, Components.CONTAINS);
-
-
+                TestGraphQueries.knapsack(db, noOfDataSystems);
                 tx.success();
             }
 
             Manager manager = new Manager(1000, db);
             manager.go();
-//            System.out.println(db.execute(viewGraph).resultAsString());
         }
-
     }
-
-    @Test
-    public void singleTest() throws Throwable {
-        try (Driver driver = GraphDatabase.driver(neo4j.boltURI(), Config.build()
-                .withEncryptionLevel(Config.EncryptionLevel.NONE)
-                .toConfig());
-             Session session = driver.session()) {
-
-//            neo4j.withConfig("LockManager", "community");
-            GraphDatabaseService db = neo4j.getGraphDatabaseService();
-
-            // create program
-            int noOfDataSystems = 1000;
-            try (Transaction tx = db.beginTx()) {
-
-                // scopes
-                Node main = db.createNode(Components.SCOPE);
-                Node computation = db.createNode(Components.SCOPE, Label.label("Computation"));
-
-                // contexts
-                Node initializer = db.createNode(Components.CONTEXT);
-                initializer.setProperty(Components.function, "INITIALIZE");
-                initializer.setProperty(Components.s1Query, "(:Uninitialized)");
-                initializer.setProperty(Components.s2Query, "(:Computation)");
-
-                Node binMutate = db.createNode(Components.CONTEXT);
-                binMutate.setProperty(Components.function, "BINARYMUTATE");
-                binMutate.setProperty(Components.s1Query, "(:Initialized)");
-                binMutate.setProperty(Components.s2Query, "(:Initialized)");
-
-                Node onePointCross = db.createNode(Components.CONTEXT);
-                onePointCross.setProperty(Components.function, "ONEPOINTCROSS");
-                onePointCross.setProperty(Components.s1Query, "(:Initialized)");
-                onePointCross.setProperty(Components.s2Query, "(:Initialized)");
-
-                Node uniformCross = db.createNode(Components.CONTEXT);
-                uniformCross.setProperty(Components.function, "UNIFORMCROSS");
-                uniformCross.setProperty(Components.s1Query, "(:Initialized)");
-                uniformCross.setProperty(Components.s2Query, "(:Initialized)");
-
-                Node output = db.createNode(Components.CONTEXT);
-                output.setProperty(Components.function, "OUTPUT");
-                output.setProperty(Components.s1Query, "(:Fittest)");
-                output.setProperty(Components.s2Query, "(:Initialized)");
-
-                main.createRelationshipTo(output, Components.CONTAINS);
-                main.createRelationshipTo(initializer, Components.CONTAINS);
-                main.createRelationshipTo(computation, Components.CONTAINS);
-
-                computation.createRelationshipTo(binMutate, Components.CONTAINS);
-                computation.createRelationshipTo(onePointCross, Components.CONTAINS);
-                computation.createRelationshipTo(uniformCross, Components.CONTAINS);
-
-                // data nodes
-                for (int i = 0; i < noOfDataSystems; i++) {
-                    Node data = db.createNode(Label.label("Data"), Label.label("Uninitialized"));
-                    main.createRelationshipTo(data, Components.CONTAINS);
-                }
-
-                // fittest solution
-                Node fittest = db.createNode(Label.label("Data"), Label.label("Fittest"));
-                main.createRelationshipTo(fittest, Components.CONTAINS);
-
-
-                tx.success();
-            }
-
-
-//            Single.go(db);
-//            System.out.println(db.execute(viewGraph).resultAsString());
-        }
-
-    }
-
 }
